0e6150b50f997dd05039d39b57be815edab79d8e
eca12v <ericanderson@Erics-MacBook-Air.local>
2016-05-24 19:34:16 -0500

    first classrrom commit

---
+++ scripts/myJavascript.js
@@ -0,0 +1,6 @@
+var replyFunct = function(){
+	console.log("button clicked");
+	document.getElementById("buttonIn").innerHTML = "Button Clicked!";
+	document.getElementById("buttonIn").style.color = "red";
+	document.getElementById("buttonIn").style.backgroundColor = "purple";
+};
